efbc87ddcda1d9e69753814eb2a1016fecd66255
Sreeju7733 <moundadanpdn@gmail.com>
2025-05-07 23:34:29 +0530

    Final Commit

--- view_sleep_log.php
+++ view_sleep_log.php
@@ -129,7 +129,6 @@ $result->data_seek(0);
         }
     });
 </script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
